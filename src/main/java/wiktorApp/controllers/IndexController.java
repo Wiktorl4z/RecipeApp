2f54d1ae873145a86dedf3fa59844793c015c163
Wiktor <wiktorkalinowski@gmail.com>
2017-11-02 17:49:58 +0100

    Lombok add

--- src/main/java/wiktorApp/controllers/IndexController.java
+++ src/main/java/wiktorApp/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package wiktorApp.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import wiktorApp.service.RecipeService;
 
 @Controller
+@Slf4j
 public class IndexController {
 
     private final RecipeService recipeService;
