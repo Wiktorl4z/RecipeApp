315689e0db38e2c2b97e6863bc70ccb61da161c6
Wiktor <wiktorkalinowski@gmail.com>
2017-11-09 23:32:24 +0100

    Add Enumeration

--- src/main/java/wiktorApp/controllers/IndexController.java
+++ src/main/java/wiktorApp/controllers/IndexController.java
@@ -1,10 +1,13 @@
 package wiktorApp.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import wiktorApp.service.RecipeService;
 
+
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -16,6 +19,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting Index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
